385e768e0f51a240fe76417da3a57dac3a3100f1
Falnésio Ghander Soares Borges <36005374+Falnesio@users.noreply.github.com>
2021-05-27 20:43:29 +0000

    Adequar página principal do site em construção

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react"
 import logo from "./logo.svg"
+import icone from "./icone.jpg"
 import "./App.css"
+ 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={icone} className="App-logo" alt="logo" />
           <div>
             <p>Contato: </p>
             <a href="mailto: social@fuzue.art">
