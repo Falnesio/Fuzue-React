3f64e8bbd39f1298d472733abb33631473f5b05e
Falnésio Ghander Soares Borges <36005374+Falnesio@users.noreply.github.com>
2021-05-27 13:23:22 +0000

    Alterar página principal

--- src/App.js
+++ src/App.js
@@ -38,9 +38,11 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Contato: social@fuzue.art
           </p>
-          <LambdaDemo />
+          <p>
+            Site em Construção
+          </p>
         </header>
       </div>
     )
