3999443618bdb151360091fc8526c72757d46311
Falnésio Ghander Soares Borges <36005374+Falnesio@users.noreply.github.com>
2021-05-27 12:32:15 +0000

    Alterar para site em construção

--- src/App.js
+++ src/App.js
@@ -38,9 +38,12 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Contato:
+            social@fuzue.art
+
+            Site em construção
           </p>
-          <LambdaDemo />
+          /* <LambdaDemo /> */
         </header>
       </div>
     )
